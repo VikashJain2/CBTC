d3b1cba26d19c65a8843fd7cd1dee6be790e2f47
Vikash Jain <141486717+VikashJain2@users.noreply.github.com>
2024-08-14 08:54:20 +0530

    Update Event.jsx

--- frontend/src/components/Event/Event.jsx
+++ frontend/src/components/Event/Event.jsx
@@ -1,24 +1,41 @@
-import React, { useContext } from 'react'
-import { events } from '../../assets/assets'
-import Card from '../Card/Card'
-import "./Event.css"
-import { eventContext } from '../../context/eventContext'
+import React, { useContext } from "react";
+import { events } from "../../assets/assets";
+import Card from "../Card/Card";
+import "./Event.css";
+import { eventContext } from "../../context/eventContext";
+import Loader from "../Loader";
 const Event = () => {
-  const {allEvents} = useContext(eventContext)
+  const { allEvents, loading } = useContext(eventContext);
   return (
-    <div className='event'>
-      
-      <div className="event-heading"><h2>Trusted By <br /> Thousand of Events</h2></div>
-      
-      <div className="event-container">
-        {
-            allEvents.map((item,index)=>{
-                return <Card key={index} img={item.image} price={item.price} date={item.startDateTime} title={item.title} category={item.category} isFree={item.isFree} organizer={item.organizer} id={item._id}/>
-            })
-        }
+    <div className="event">
+      <div className="event-heading">
+        <h2>
+          Trusted By <br /> Thousand of Events
+        </h2>
       </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="event-container">
+          {allEvents.map((item, index) => {
+            return (
+              <Card
+                key={index}
+                img={item.image}
+                price={item.price}
+                date={item.startDateTime}
+                title={item.title}
+                category={item.category}
+                isFree={item.isFree}
+                organizer={item.organizer}
+                id={item._id}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Event
+export default Event;
